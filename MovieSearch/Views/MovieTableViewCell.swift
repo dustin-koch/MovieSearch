e3d4691f4fa5671a41413d8cc4beb74da3f7b226
Dustin Koch <dustin.koch@me.com>
2019-05-17 09:15:12 -0600

    initial commit: Storyboard laid out

---
+++ MovieSearch/Views/MovieTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieTableViewCell.swift
+//  MovieSearch
+//
+//  Created by Dustin Koch on 5/17/19.
+//  Copyright © 2019 Rabbit Hole Fashion. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var moviePosterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    
+}//END OF CLASS
